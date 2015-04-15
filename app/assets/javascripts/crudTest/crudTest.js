c73a5526a8cdec6d29273e43b82fa64e03572ec2
Skessler <stvnksslr@gmail.com>
2015-04-15 11:48:24 -0400

    require syntax changes to crudTest

--- app/assets/javascripts/crudTest/crudTest.js
+++ app/assets/javascripts/crudTest/crudTest.js
@@ -13,49 +13,49 @@
         module.factory('UserService', ['$http',
             function($http) {
 
-                function UserService() {
-                }
+                function UserService() {}
 
-                UserService.createUser = function (userData) {
+                UserService.createUser = function(userData) {
                     var createUserPromise = $http.post('/user', userData);
                     return createUserPromise;
                 };
-                UserService.findUsers = function () {
+                UserService.findUsers = function() {
                     var findUsersPromise = $http.get('/users');
                     console.log('Steve Debug: ' + findUsersPromise);
                     return findUsersPromise;
                 };
                 return UserService;
-            }]);
+            }
+        ]);
 
         function MyController(UserService) {
 
-                    // load all users when this controller loads
-                    // then you can do shit with them in code or hmtl
-                    // eg: <li ng-repeat="user in users">{{user.firstName}} {{user.lastName}}</li>
-                    UserService.findUsers().then(function (response) {
-                        var users = response.data; // this assumes that response.data is the array of objects, cant tell from what you have shown me
-                        this.users = users;
-                    }.bind(this));
+            // load all users when this controller loads
+            // then you can do shit with them in code or hmtl
+            // eg: <li ng-repeat="user in users">{{user.firstName}} {{user.lastName}}</li>
+            UserService.findUsers().then(function(response) {
+                var users = response.data; // this assumes that response.data is the array of objects, cant tell from what you have shown me
+                this.users = users;
+            }.bind(this));
 
-                    // if you call this from html or elsewhere, it will create new user
-                    // eg <button ng-click="new user()"></button>
-                    this.createNewUser = function () {
+            // if you call this from html or elsewhere, it will create new user
+            // eg <button ng-click="new user()"></button>
+            this.createNewUser = function() {
 
-                        var newUser = {
-                            firstName: 'Steven',
-                            lastName: 'Kessler'
-                        };
+                var newUser = {
+                    firstName: 'Steven',
+                    lastName: 'Kessler'
+                };
 
-                        UserService.createUser(newUser).then(function (response) {
-                            var createNewUser = response.data;
-                            console.log('created newUser', newUser);
-                            this.createNewUser = createNewUser;
-                        });
+                UserService.createUser(newUser).then(function(response) {
+                    var createNewUser = response.data;
+                    console.log('created newUser', newUser);
+                    this.createNewUser = createNewUser;
+                });
 
-                    };
-                    module.controller('MyController', ['UserService',MyController]);
-                }
-                 return module;
+            };
+            module.controller('MyController', ['UserService', MyController]);
+        }
+        return module;
     });
 })();
